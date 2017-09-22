52b4968336f8800c8531951b457bf44d38a62492
Noah Abrahamson <nabrahamson@gmail.com>
2017-09-22 18:54:23 -0500

    Create working react base

---
+++ client/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div style={{textAlign: 'center'}}>
+        <header>Something something something darkside</header>
+      </div>);
+  }
+}
+
